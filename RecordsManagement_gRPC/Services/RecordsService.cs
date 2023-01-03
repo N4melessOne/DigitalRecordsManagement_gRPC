bd3ccd069884656460c1bb82ad99c820d2519016
N4melessOne <gamerlacikaaa@gmail.com>
2023-01-03 14:27:22 +0100

    Implemented and tested insert on server side, but double seems to be a problem (comma is passed instead of point)

--- RecordsManagement_gRPC/Services/RecordsService.cs
+++ RecordsManagement_gRPC/Services/RecordsService.cs
@@ -12,7 +12,7 @@ namespace RecordsManagement_gRPC.Services
             this.logger = logger;
         }
 
-        public override async Task AllRecords(allRecordsRequest request, 
+        public override async Task AllRecords(allRecordsRequest request,
             IServerStreamWriter<recordModel> responseStream, ServerCallContext context)
         {
             using (SqlConnection connection = RecordsDbConntectionService.GetConnection())
@@ -21,20 +21,27 @@ namespace RecordsManagement_gRPC.Services
 
                 using (SqlCommand command = new SqlCommand(sql, connection))
                 {
-                    connection.Open();
-                    using (SqlDataReader reader = command.ExecuteReader())
+                    try
                     {
-                        while (reader.Read())
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
-                            recordModel model = new recordModel();
-                            model.RecordId = reader.GetInt32(0);
-                            model.Performer = reader.GetString(1);
-                            model.Title = reader.GetString(2);
-                            model.Price = reader.GetFloat(3);
-                            model.StockCount = reader.GetInt32(4);
+                            while (reader.Read())
+                            {
+                                recordModel model = new recordModel();
+                                model.RecordId = reader.GetInt32(0);
+                                model.Performer = reader.GetString(1);
+                                model.Title = reader.GetString(2);
+                                model.Price = reader.GetDouble(3);
+                                model.StockCount = reader.GetInt32(4);
 
-                            await responseStream.WriteAsync(model);
+                                await responseStream.WriteAsync(model);
+                            }
                         }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
                     }
                 }
             }
@@ -69,5 +76,45 @@ namespace RecordsManagement_gRPC.Services
                 }
             }
         }
+
+        public override Task<responseModel> AddRecord(NewRecord request, ServerCallContext context)
+        {
+            responseModel response = new responseModel();
+
+            using (SqlConnection connection = RecordsDbConntectionService.GetConnection())
+            {
+                string sql = $"INSERT INTO [dbo].[Record] (Performer, Title, Price, StockCount) VALUES ('{request.Performer}', '{request.Title}', {request.Price}, {request.StockCount})";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        int affectedRows = command.ExecuteNonQuery();
+                        if (affectedRows > 0)
+                        {
+                            response.Error = 0;
+                            response.Message = "Successfully inserted new record!";
+                        }
+                        else
+                        {
+                            response.Error = 1;
+                            response.Message = "Insert of new record failed!";
+                        }
+
+                        return Task.FromResult(response);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        response.Error = 1;
+                        response.Message = "Insert of new record failed!";
+                        return Task.FromResult(response);
+                    }
+                }
+            }
+        }
+
+
     }
 }
--- RecordsServiceTest/RecordsServiceTest.cs
+++ RecordsServiceTest/RecordsServiceTest.cs
@@ -7,6 +7,8 @@ namespace RecordsServiceTest
     [TestClass]
     public class RecordsServiceTest
     {
+        private static GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:8888");
+
         [TestMethod]
         public void TestConnection()
         {
@@ -29,10 +31,74 @@ namespace RecordsServiceTest
                 StockCount = 18
             };
 
-            var channel = GrpcChannel.ForAddress("https://localhost:8888");
             var testClient = new RecordsManagementService.RecordsManagementServiceClient(channel);
 
             recordModel actual = testClient.GetRecordById(new IdOfRecord { RecordId = 3 });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public async Task TestAllRecords()
+        {
+            List<recordModel> expected = new List<recordModel>();
+            using (SqlConnection connection = RecordsDbConntectionService.GetConnection())
+            {
+                string sql = "SELECT * FROM [dbo].Record";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            recordModel model = new recordModel();
+                            model.RecordId = reader.GetInt32(0);
+                            model.Performer = reader.GetString(1);
+                            model.Title = reader.GetString(2);
+                            model.Price = reader.GetDouble(3);
+                            model.StockCount = reader.GetInt32(4);
+                            expected.Add(model);
+                        }
+                    }
+                }
+            }
+
+            List<recordModel> actual = new List<recordModel>();
+
+            var testClient = new RecordsManagementService.RecordsManagementServiceClient(channel);
+
+            using (var call = testClient.AllRecords(new allRecordsRequest()))
+            {
+                while (await call.ResponseStream.MoveNext(new CancellationToken()))
+                {
+                    actual.Add(call.ResponseStream.Current);
+                }
+            }
+
+            Assert.AreEqual(expected.Count, actual.Count);
+        }
+
+        [TestMethod]
+        public void TestNewRecord()
+        {
+            responseModel expected = new responseModel
+            {
+                Error = 0,
+                Message = "Successfully inserted new record!"
+            };
+
+            var testClient = new RecordsManagementService.RecordsManagementServiceClient(channel);
+
+            NewRecord newRecord = new NewRecord()
+            {
+                Performer = "Like Moths To Flames",
+                Title = "No Eternety in Gold",
+                Price = 12,
+                StockCount = 6
+            };
+            responseModel actual = testClient.AddRecord(newRecord);
 
             Assert.AreEqual(expected, actual);
         }
